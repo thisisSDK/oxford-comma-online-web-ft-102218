acf9cd65d4cff1085535cb80d1230a5224064bca
sdk1270 <sdk1270@gmail.com>
2018-10-24 18:32:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
     if array.length ==2 
       return "#{array[0]} and #{array[1]}" #the array has 2 componenets, .join("and")
     elsif 2 < array.length 
-      array[-1].insert (0,"and") #the array has 3 componenets, .join("," , "and")
+      array[-1].insert (0 "and") #the array has 3 componenets, .join("," , "and")
   end
     array.join(",")
 end
