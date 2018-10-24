2916faa5322f4c78035cf0ba78a9bf96312cbdda
sdk1270 <sdk1270@gmail.com>
2018-10-24 18:36:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,9 @@
 
 def oxford_comma(array)
   if array.length == 2 
-      return "#{array[0]} and #{array[1]}" #the array has 2 componenets, .join("and")
+      return "#{array[0]} and #{array[1]}"                       #the array has 2 componenets, .join("and")
     elsif 2 < array.length 
-      array[-1].insert(0, "and ") #the array has 3 componenets, .join("," , "and")
+      array[-1].insert(0, "and ")                        #the array has 3 componenets, .join("," , "and")
   end
     array.join(",")
-end
-
-def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
-  end
-  array.join(", ")    
 end
