3370923f71e80880c41219be2de10a9afc8ebf71
sdk1270 <sdk1270@gmail.com>
2018-10-24 18:39:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -11,11 +11,3 @@ end
 
 
 
-def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
-  end
-  array.join(", ")    
-end
