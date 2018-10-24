3c1cb1a4145415a5f6280c5c838133e8ba7421a5
sdk1270 <sdk1270@gmail.com>
2018-10-24 18:34:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,24 @@
+
 def oxford_comma(array)
-    if array.length ==2 
-      return "#{array[0]} and #{array[1]}" #the array has 2 componenets, .join("and")
-    elsif 2 < array.length 
-      array[-1].insert (",", "and") #the array has 3 componenets, .join("," , "and")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
   end
-    array.join(",")
+  array.join(", ")    
 end
+
+
+
+
+
+
+#def oxford_comma(array)
+ #   if array.length ==2 
+  #    return "#{array[0]} and #{array[1]}" #the array has 2 componenets, .join("and")
+   # elsif 2 < array.length 
+    #  array[-1].insert (",", "and") #the array has 3 componenets, .join("," , "and")
+  #end
+  #  array.join(",")
+#end
 
